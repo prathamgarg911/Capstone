3d03c178bd802a31d04837bfd30c9971de92803d
Kapooryashmit512 <kapooryashmit512@gmail.com>
2024-04-25 11:47:16 +0530

    Added frontend files

---
+++ capstone/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SeatMatrix from './SeatMatrix';
+
+function App() {
+  return (
+    <div className="App">
+      <SeatMatrix />
+    </div>
+  );
+}
+
+export default App;
---
+++ capstone/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'; // You can create index.css for global styles if needed
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
